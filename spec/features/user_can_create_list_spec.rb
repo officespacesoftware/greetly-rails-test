9de359f1b0551718361eb8038e2dbe206f9a3d98
SyntheticAutomation <syntheticauto900@gmail.com>
2019-08-31 13:58:21 -0600

    Adds test for creating lists

--- spec/features/user_can_create_list_spec.rb
+++ spec/features/user_can_create_list_spec.rb
@@ -7,6 +7,12 @@ describe 'A user who visits the app' do
 
     click_on 'New List'
     expect(current_path).to eq(new_list_path)
+
+    fill_in :list_title, with: "Big Financial Decisions"
+    click_on "Create List"
+    expect(current_path).to eq(list_path(List.last))
+    expect(page).to have_content('Big Financial Decisions')
   end
+  # -------------------------------------
 end
 
