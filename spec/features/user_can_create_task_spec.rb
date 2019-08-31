d6bf655e547d2f10abe328c1c4d0c12a006cc027
SyntheticAutomation <syntheticauto900@gmail.com>
2019-08-31 15:06:29 -0600

    Adds test for task creation with due date

--- spec/features/user_can_create_task_spec.rb
+++ spec/features/user_can_create_task_spec.rb
@@ -1,1 +1,31 @@
 require 'spec_helper'
+
+describe 'A user who visits the app' do
+  # -------------------------------------
+  it 'can create a task' do
+    list_1 = List.create(title: "Dogs I want")
+
+    
+    visit list_path(list_1.id)
+    expect(page).to have_content('Dogs I want')
+
+    click_on "Tasks"
+    expect(current_path).to eq(list_tasks_path(list_1.id))
+
+    click_on "New Task"
+    expect(current_path).to eq(new_list_task_path(list_1.id))
+
+    fill_in :task_name, with: "Poodle"
+    fill_in :task_due_date, with: "2022-12-25"
+
+    click_on "Create Task"
+    expect(current_path).to eq("/lists/#{list_1.id}/tasks/#{Task.last.id}")
+
+    click_on "Back"
+    expect(current_path).to eq(list_tasks_path(list_1.id))
+    expect(page).to have_content("Poodle")
+    expect(page).to have_content("2022-12-25")
+  end
+  # -------------------------------------
+end
+
