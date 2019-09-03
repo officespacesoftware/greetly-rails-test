e77567052e328f81fbb67b834d581b6703fb787f
bghalami <benjaminghalami@gmail.com>
2019-09-03 16:18:26 -0600

    Added ability to add due_date from task create page

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ActiveRecord::Base
   belongs_to :list
-  attr_accessible :complete, :name
+  attr_accessible :complete, :name, :due_date
 
   validates :name, presence: true
+  validates :due_date, presence: true
 end
