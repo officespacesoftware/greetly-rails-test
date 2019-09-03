d328e2edc8375e17975be9f2c0d00e85021e9d68
Vince Carollo <vincecarollo@gmail.com>
2019-09-03 16:25:04 -0600

    Add list and due_date to attr_accessible in tasks model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,9 @@
 class Task < ActiveRecord::Base
   belongs_to :list
-  attr_accessible :complete, :name
+  attr_accessible :complete,
+                  :name,
+                  :list,
+                  :due_date
 
   validates :name, presence: true
 end
