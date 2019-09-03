3fbc0e398602207e68f17ef229d0d1979bc2c052
Vince Carollo <vincecarollo@gmail.com>
2019-09-03 16:25:16 -0600

    Add test for adding a due date to a task

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -7,4 +7,17 @@ describe Task do
       expect(subject.valid?).to be_false
     end
   end
+
+  context "due_date" do
+    it "can add a due date" do
+      list = List.create!(title: 'list1')
+      task = Task.create!(name: 'task1', complete: false, list: list)
+
+      due_date = Time.now + 3.days
+
+      task.update_attributes(due_date: due_date)
+
+      expect(task.due_date).to eq(due_date)
+    end
+  end
 end
