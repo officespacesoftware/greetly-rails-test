eb07189915a759b283cb0957350ce5ec7cd107f4
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-10 14:08:32 -0600

    updated schema with new migration

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(:version => 20190910185624) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.date     "due_date"
-  end
+  end 
 
   add_index "tasks", ["list_id"], :name => "index_tasks_on_list_id"
 
