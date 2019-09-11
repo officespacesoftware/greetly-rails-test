e4072c09a8e790d44917a33fb41f0863670b82de
Brenden Thornton <thorntonbrenden@gmail.com>
2019-09-10 22:58:47 -0600

    removed code that was no longer needed

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,17 +3,4 @@ class Task < ActiveRecord::Base
   attr_accessible :complete, :name, :due_date
 
   validates :name, presence: true
-
-  # scope :order_by_due_date, -> { self.sort_by {|task| task.due_date} }
-
-  # scope :order_by_year, lambda {|task| order(year: :desc) }
-  # #   { order(year: :desc) }
-  # # end
-  #
-  # scope :by_name, lambda{ |name| where(name: name) unless name.nil? }
-
-  def sort_due_date
-    self.sort_by {|task| task.due_date}
-  end
-
 end
